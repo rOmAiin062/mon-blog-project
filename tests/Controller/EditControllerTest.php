18544c73b352cf42d8b14dfe7a1b61cd0c19d170
romaiin062 <rom62575@gmail.com>
2019-02-12 16:12:30 +0100

    feat: work on tests - new article

--- tests/Controller/EditControllerTest.php
+++ tests/Controller/EditControllerTest.php
@@ -1,21 +1,21 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: romain
- * Date: 02/01/19
- * Time: 16:50
- */
-
-
-namespace App\Tests\Controller;
-
-use  \Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class EditControllerTest extends WebTestCase
-{
-
-    public function edit()
-    {
-
-    }
-}
+///**
+// * Created by IntelliJ IDEA.
+// * User: romain
+// * Date: 02/01/19
+// * Time: 16:50
+// */
+//
+//
+//namespace App\Tests\Controller;
+//
+//use  \Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+//
+//class EditControllerTest extends WebTestCase
+//{
+//
+//    public function edit()
+//    {
+//
+//    }
+//}
